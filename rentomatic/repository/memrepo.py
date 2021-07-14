0a87d8287d848efa6539a6b0424f4fc2fe9f6736
Stiffer Doroskevich <stiffer17@gmail.com>
2021-07-13 20:10:12 -0400

    FEATURE: Added Repository implementarion + test

--- rentomatic/repository/memrepo.py
+++ rentomatic/repository/memrepo.py
@@ -1,2 +1,9 @@
+from rentomatic.domain.room import Room
+
+
 class MemRepo:
-    pass
+    def __init__(self, data):
+        self.data = data
+
+    def list(self):
+        return [Room.from_dict(i) for i in self.data]
